294446e3054f83673c3565c5bf0ff362569b2af2
guowei.dong <dongguowei@caimiao.com>
2015-08-04 13:24:32 +0800

    Signed-off-by: guowei.dong <dongguowei@caimiao.com>

--- 游戏画图/pixi.js-master/src/polyfill/index.js
+++
@@ -1,2 +0,0 @@
-require('./Object.assign.js');
-require('./requestAnimationFrame');
--- 游戏画图/svg.js-master/src/selector.js
+++
@@ -1,5 +0,0 @@
-// Method for getting an element by id
-SVG.get = function(id) {
-  var node = document.getElementById(idFromReference(id) || id)
-  if (node) return node.instance
-}
